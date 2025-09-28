d79e5762e23e4dc6a7acecc8290cb5679441fe8f
Furqan <mdfurqanahmadda2@gmail.com>
2025-09-29 03:50:28 +0530

    Refactor stopwatch logic to streamline state management and enhance control buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,18 @@ import { useState, useEffect } from "react";
 export function App() {
   const [timer, setTimer] = useState(0);
   const [speed, setSpeed] = useState(1000);
-  const [isStart, setisStart] = useState(false);
-  const [isStopped, setisStopped] = useState(true);
-  const [showSpeed, setShowSpeed] = useState(undefined);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    let interval_id = undefined;
-    if (!isStopped && isStart) {
-      interval_id = setInterval(() => {
-        setTimer((prevTime) => prevTime + 1);
-      }, speed);
-    }
+    if (!isRunning) return;
+    const interval_id = setInterval(() => {
+      setTimer((prevTime) => prevTime + 1);
+    }, speed);
 
     return () => {
       clearInterval(interval_id);
     };
-  }, [speed, isStopped, isStart]);
+  }, [speed, isRunning]);
 
   return (
     <>
@@ -29,61 +25,62 @@ export function App() {
         >
           <div>
             <button
-              className="myBtn text-blue-400"
-              onClick={() => {
-                setSpeed(speed / 2);
-                setShowSpeed(true);
-              }}
+              className="myBtn text-lg text-blue-400"
+              onClick={() => setSpeed(speed / 2)}
             >
               Increase speed
             </button>
-            {showSpeed && <p>{1000 / speed}x</p>}
+            
+          </div>
+
+          <div>
+            <h1>{timer} sec</h1>
           </div>
 
           <div>
             <button
-              className="myBtn text-green-400"
-              onClick={() => {
-                setisStart(true);
-                setisStopped(false);
-              }}
-            >
-              Start
-            </button>
-          </div>
-
-          <div>
-            <h1>{timer}</h1>
-          </div>
-
-          <div>
-            <button
-              className="myBtn text-red-400"
-              onClick={() => {
-                setisStopped(true);
-                setisStart(false);
-              }}
-            >
-              Stop{" "}
-            </button>
-          </div>
-          <div>
-            <button
-              className="myBtn text-yellow-400"
-              onClick={() => {
-                setSpeed(speed * 2);
-                setShowSpeed("");
-              }}
+              className="myBtn text-lg text-yellow-400"
+              onClick={() => setSpeed(speed * 2)}
             >
               Decrease speed
             </button>
-            {showSpeed === "" ? <p>- {1000 / speed}x</p> : ""}
+            
           </div>
+
+          {!isRunning && (
+            <div>
+              <button
+                className="myBtn text-lg text-green-400"
+                onClick={() => setIsRunning(true)}
+              >
+                Start
+              </button>
+            </div>
+          )}
+
+          {isRunning && (
+            <div>
+              <button
+                className="myBtn text-lg text-red-400"
+                onClick={() => setIsRunning(false)}
+              >
+                Pause
+              </button>
+            </div>
+          )}
           <div>
-            <button className="myBtn text-red-400" onClick={() => setTimer(0)}>
-              Reset{" "}
+            <button
+              className="myBtn text-lg"
+              onClick={() => {
+                setTimer(0);
+                setSpeed(1000);
+                setIsRunning(false);
+              }}
+            >
+              Reset
             </button>
           </div>
+        <div>{speed !== 1000 ? <p>{1000 / speed}x</p> : ""}</div>
         </div>
       </div>
     </>
